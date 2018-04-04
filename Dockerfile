b2c41e8ab351866c30e2504004241dbf6374ed9a
Denis Kramer <d.kramer@soton.ac.uk>
2018-04-04 19:49:55 +0100

    Fixed vncd to circumvent bug in libvncserver
    There is a bug in libvncserver that leads to a buffer overflow if no valid
    IP V6 port is available. Now we assign a V6 port explicitly to aviod x11vnc
    trying to allocate a used port.

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,9 @@ RUN yum -y update && \
     yum -y install urw-fonts && \
     yum clean all
 
+# Install vncproxy
+COPY assets/vncd /sbin/vncd
+
 # Configure X11 to run headless w/o graphics card
 # Note: edit assets/xorg.conf to change resolution
 COPY assets/xorg.conf /etc/X11/xorg.conf
@@ -20,6 +23,9 @@ COPY assets/sddm.conf /etc/sddm.conf
 
 # Install openbox assets
 COPY assets/openbox/menu.xml /etc/xdg/openbox/menu.xml
+
+# Install VNC boostrap
+COPY assets/startvnc.sh /etc/vncd/startvnc.sh
 
 # Copy entrypoint
 COPY assets/entrypoint.sh /entrypoint.sh
--- assets/startvnc.sh
+++ assets/startvnc.sh
@@ -4,15 +4,19 @@
 # It receives a port number as first argument on which the VNC
 # server should listen.
 
-while [[ ${1:0:1} == - ]]; do
-  [[ $1 =~ ^-h|--help ]] && {
-    cat <<-EOF
-USAGE: $(basename $0) [OPTIONS] [TEXT]
+function usage() {
+cat <<-EOF
+USAGE: $(basename $0) [OPTIONS] [PORT] [PORTV6]
 
 OPTIONS
 
-  -h  Display help message
+-h  Display help message
 EOF
+}
+
+while [[ ${1:0:1} == - ]]; do
+  [[ $1 =~ ^-h|--help ]] && {
+    usage
     exit;
   };
 
@@ -21,7 +25,14 @@ EOF
   break;
 done
 
+if [ "$#" -lt 2 ]; then
+  usage
+  exit
+fi
+
 PORT=$1
+PORTV6=$2
+
 DISPLAY=""
 
 FBFILE=$(mktemp /tmp/.vncbootstrap-fb-XXXXXX)
@@ -31,11 +42,12 @@ AUTHSOCKET=$(mktemp /tmp/.vncbootstrap-auth-XXXXXX)
 exec 6<> ${FBFILE}
 /usr/bin/X -displayfd 6 -auth ${AUTHSOCKET} &
 exec 6<&- # close file
-while [ ${DISPLAY} == "" ]; do
+
+while [[ ${DISPLAY} == "" ]]; do
   DISPLAY=$(cat ${FBFILE})
 done
 echo "X server display: ${DISPLAY}"
 
 # Start VNC Server
-/usr/bin/x11vnc -xkb -noxrecord -noxfixes -noxdamage -rfbport ${PORT} \
-                -display ${DISPLAY} -auth ${AUTHSOCKET} -ncache 10
+/usr/bin/x11vnc -xkb -noxrecord -noxfixes -noxdamage -rfbport ${PORT} -rfbport ${PORTV6} \
+                -display :${DISPLAY} -auth ${AUTHSOCKET} -ncache 10
